7211a2c93bb1854e5b53e3068ea6afd724dac5c0
yusuf idris ceylan <y.idris3471@gmail.com>
2024-07-04 21:31:53 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int kenar1, kenar2;
+        double hipotenus;
 
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("kenar1'i giriniz: ");
+        kenar1 = input.nextInt();
 
+        System.out.print("kenar2'yi giriniz: ");
+        kenar2 = input.nextInt();
 
-        }
+        hipotenus = Math.sqrt(Math.pow(kenar1, 2) + Math.pow(kenar2, 2));
+        System.out.println("hipotenus: " + hipotenus);
+
+        input.close();
     }
 }
